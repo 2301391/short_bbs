ebc74d3906e87587392d5355c932b64f0f575d16
2301391 <2301391@s.asojuku.ac.jp>
2025-06-02 10:44:41 +0900

    a3

---
+++ login.php
@@ -0,0 +1,9 @@
+
+<h1>ログイン</h1>
+
+
+<form action="check.php" method="post">
+        <p>ユーザー名：<input type="text" name="username" required></p>
+        <p>パスワード：<input type="text" name="password" required></p>
+        <p><button type="submit">ログイン</button></p>
+</form>
